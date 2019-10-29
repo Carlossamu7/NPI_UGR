91950ad93b30642709bcdc839bb0bc94dfe63de5
carlossamu7 <carlossamu7@gmail.com>
2019-10-29 23:49:37 +0100

    Versión java lista

---
+++ app/src/main/java/com/example/alhambra/War.java
@@ -0,0 +1,14 @@
+package com.example.alhambra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class War extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_war);
+    }
+}
