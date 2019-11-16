7ac5b75a23ba1e79a35cff9c924e0d4d6b057036
Dr.Oak <miguerobles@correo.ugr.es>
2019-11-16 17:48:15 +0100

    Added Swap option and media

--- app/src/main/java/com/example/alhambra/War.java
+++ app/src/main/java/com/example/alhambra/War.java
@@ -7,26 +7,88 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
+
 public class War extends AppCompatActivity implements SensorEventListener {
 
+    private TextView text;
     private SensorManager sensorManager;
     private Sensor acelerometro;
     private int disparo = 0;
     private int recarga = 0;
+    private int swapping = 0;
+    private int precision = 8;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_war);
+        text = (TextView)findViewById(R.id.textAcelerometro);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         assert sensorManager != null;
         acelerometro = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         assert acelerometro != null;
+    }
+
+    public void reload(float x){
+
+        if(x<=-precision && recarga == 0){
+            recarga++;
+        }
+        else if (x>-precision && recarga == 1){
+            recarga++;
+        }
+        else if(recarga == 2) {
+            recarga++;
+            soundReload();
+            text.setText("");
+            text.append("Shoot");
+        }
+    }
+
+    public void shoot(float x){
+
+        if(x<=precision && disparo == 0){
+            disparo++;
+            getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+        }
+        else if (x>precision && disparo == 1){
+            disparo++;
+            getWindow().getDecorView().setBackgroundColor(Color.GRAY);
+
+        }
+        else if(disparo == 2) {
+            disparo = 0;
+            recarga = 0;
+            soundShot();
+            text.setText("");
+            text.append("Reload");
+        }
+    }
+
+    public void swap(float y){
+
+        if(y<=precision && swapping == 0){
+            swapping++;
+            getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+        }
+        else if (y>precision && swapping == 1){
+            swapping++;
+            getWindow().getDecorView().setBackgroundColor(Color.GRAY);
+
+        }
+        else if(swapping == 2) {
+            swapping = 0;
+            soundSwap();
+            text.setText("");
+            text.append("Swapped");
+        }
     }
 
     @Override
@@ -40,32 +102,10 @@ public class War extends AppCompatActivity implements SensorEventListener {
         y = event.values[1];
         z = event.values[2];
 
-        if(x<=7 && disparo == 0 && recarga == 3){
-            disparo++;
-            getWindow().getDecorView().setBackgroundColor(Color.BLUE);
-        }
-        else if (x>7 && disparo == 1 && recarga == 3){
-            disparo++;
-            getWindow().getDecorView().setBackgroundColor(Color.RED);
+        swap(y);
+        reload(x);
+        if(recarga == 3) shoot(x);
 
-        }
-        if(disparo == 2) {
-            disparo = 0;
-            recarga = 0;
-            soundShot();
-        }
-
-        if(x<=-7 && recarga ==0){
-            recarga++;
-            getWindow().getDecorView().setBackgroundColor(Color.BLUE);
-        }
-        else if (x>-7 && recarga == 1){
-            recarga++;
-        }
-        if(recarga == 2) {
-            recarga++;
-            soundReload();
-        }
     }
 
 
@@ -75,6 +115,10 @@ public class War extends AppCompatActivity implements SensorEventListener {
     }
     private void soundReload( ){
         MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.recarga);
+        mediaPlayer.start();
+    }
+    private void soundSwap( ){
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.swap);
         mediaPlayer.start();
     }
 
