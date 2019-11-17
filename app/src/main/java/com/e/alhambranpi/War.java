d16ab99f9335a0435b51eb4731b64f5dfc972779
Dr.Oak <miguerobles@correo.ugr.es>
2019-11-17 02:26:21 +0100

    Añadidos medios necesarios e implementado cambio, disparo y recarga de cada una de ellas

--- app/src/main/java/com/e/alhambranpi/War.java
+++ app/src/main/java/com/e/alhambranpi/War.java
@@ -1,6 +1,5 @@
 package com.e.alhambranpi;
 
-import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -11,25 +10,33 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Objects;
+
 import static android.os.SystemClock.sleep;
 
 public class War extends AppCompatActivity implements SensorEventListener {
 
-    private TextView text;
+    private TextView texto_arriba;
+    private TextView texto_abajo;
     private SensorManager sensorManager;
     private Sensor acelerometro;
     private int disparo = 0;
     private int recarga = 0;
     private int swapping = 0;
     private int precision = 7;
+    private double giro = 4.5;
+    private boolean arma_seleccionada = false;
+    private int arma = 0; //arco = 0, pistola = 1, cañon = 2
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_war);
-        getSupportActionBar().setTitle("Recarga y dispara!");
-        text = (TextView)findViewById(R.id.textAcelerometro);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Alhabatalla");
+        texto_arriba = (TextView)findViewById(R.id.textAcelerometro);
+        texto_abajo = (TextView)findViewById(R.id.textSwap);
+
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         assert sensorManager != null;
@@ -43,9 +50,10 @@ public class War extends AppCompatActivity implements SensorEventListener {
             recarga++;
         } else if (x < -precision && recarga == 1) {
             soundReload();
+            arma_seleccionada = true;
             recarga++;
-            text.setText("");
-            text.append("Shoot");
+            texto_abajo.setText("");
+            texto_abajo.append("¡Dispara!");
         }
     }
 
@@ -58,24 +66,22 @@ public class War extends AppCompatActivity implements SensorEventListener {
             disparo = 0;
             recarga = 0;
             soundShot();
-            text.setText("");
-            text.append("Reload");
+            arma_seleccionada = false;
+            texto_abajo.setText("");
+            texto_abajo.append("Recarga o elige tu arma");
         }
     }
 
-    public boolean swap(float y){
+    public void swap(float y, float z){
 
-        if(y<=precision && swapping == 0){
+        if(( z>= precision || y<=precision) && swapping == 0){
             swapping++;
         }
-        else if (y>precision && swapping == 1){
+        else if ((z < precision-2 || y >precision+1) && swapping == 1){
             soundSwap();
+            arma = (++arma)%3;
             swapping = 0;
-            text.setText("");
-            text.append("Swapped");
-            return true;
         }
-        return false;
     }
 
     @Override
@@ -89,26 +95,47 @@ public class War extends AppCompatActivity implements SensorEventListener {
         y = event.values[1];
         z = event.values[2];
 
-        /*
-
-        if(z>=-4.5 && z <= 4.5) {
+        if(z>= -giro && z <= giro) {
             if (recarga < 2) reload(x);
             else if (recarga == 2) shoot(x);
         }
-*/
-        if(x >= -2 && x <= 2) swap(y);
-        text.setText("");
-        text.append("\n" + "X: " + x + "\n" + "Y: " + y + "\n" +"Z: " + z);
+        else if(x >= -giro && x <= giro && !arma_seleccionada) swap(y, z);
+
+        if(arma == 0) {
+            getWindow().getDecorView().setBackgroundResource(R.drawable.arco);
+            texto_arriba.setText("");
+            texto_arriba.append("Arco");
+        }
+        else if (arma == 1) {
+            getWindow().getDecorView().setBackgroundResource(R.drawable.esco);
+            texto_arriba.setText("");
+            texto_arriba.append("Escopeta");
+        }
+        else {
+            getWindow().getDecorView().setBackgroundResource(R.drawable.cannon);
+            texto_arriba.setText("");
+            texto_arriba.append("Cañón");
+        }
+
     }
 
 
-    private void soundShot( ){
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.disparo);
+    private void soundShot(){
+        MediaPlayer mediaPlayer;
+        if(arma == 0) mediaPlayer = MediaPlayer.create(this, R.raw.disparar_arco);
+        else if (arma == 1) mediaPlayer = MediaPlayer.create(this, R.raw.disparo);
+        else  mediaPlayer = MediaPlayer.create(this, R.raw.disparar_canion);
+
         mediaPlayer.start();
     }
-    private void soundReload( ){
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.recarga);
+    private void soundReload(){
+        MediaPlayer mediaPlayer;
+        if(arma == 0) mediaPlayer = MediaPlayer.create(this, R.raw.tensar_arco);
+        else if (arma == 1) mediaPlayer = MediaPlayer.create(this, R.raw.recarga);
+        else  mediaPlayer = MediaPlayer.create(this, R.raw.encender_canion);
+
         mediaPlayer.start();
+
     }
     private void soundSwap( ){
         MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.swap);
